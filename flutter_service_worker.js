1ced1ee0f9eb4df80fd9674a8ac5930b0f098704
Alnagdy <ahmedalnagdy188@gmail.com>
2025-06-22 13:01:16 +0300

    Built web
    Branch: main
    Commit: cce44464878b6e794aa6d9aa3e7d5d4137ee69fa
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d6583c1d0536deea49cfb273eadeb294",
+const RESOURCES = {"flutter_bootstrap.js": "a904b75df4c14c40b06766c713d70e32",
 "version.json": "04a265a34776b80c11723f26a788a07c",
 "index.html": "961f8f7a97129b26838611ee62a76487",
 "/": "961f8f7a97129b26838611ee62a76487",
-"main.dart.js": "3a95dc38f201475c7738f40cd4516f3b",
+"main.dart.js": "b536f2c4188fe14c229f97a4a8294aa7",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "ee0fcc6848d2f3cca979f5447ea20208",
 "icons/Icon-192.png": "41927344688fce61bded0fd771d64dfa",
